056a642c30130a56ece0fb60b045825f2f0dd794
SeifA7mad <sauofa_ahmad@yahoo.com>
2021-09-24 13:57:01 +0200

    backimg

--- src/ui/backgroung-img/BackgroundImg.js
+++ src/ui/backgroung-img/BackgroundImg.js
@@ -1,9 +1,12 @@
 import classes from './BackgroundImg.module.css';
 
 const BackgroundImg = (props) => (
-    <div className={classes.backgroundImg}>
-        {props.children}
-    </div>
+  <div
+    className={classes.backgroundImg}
+    style={{ '--backgd-img': `` }}
+  >
+    {props.children}
+  </div>
 );
 
 export default BackgroundImg;
