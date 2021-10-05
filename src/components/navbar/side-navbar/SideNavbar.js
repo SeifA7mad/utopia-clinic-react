6b0812c03599203b2fd6ac694d53c04c4cb7afa7
SeifA7mad <sauofa_ahmad@yahoo.com>
2021-10-05 02:03:59 +0200

    sidNav

--- src/components/navbar/side-navbar/SideNavbar.js
+++ src/components/navbar/side-navbar/SideNavbar.js
@@ -1,50 +1,64 @@
 import { NavLink } from 'react-router-dom';
 import { useContext } from 'react';
-import {AuthContext} from '../../../store/auth-context';
+import { AuthContext } from '../../../store/auth-context';
 
 import IconText from '../../../ui/icons/IconText';
 import classes from './SideNavbar.module.css';
 
 const pathname = '/dashboard';
 
-const SideNavbar = () => {
-    const {userToken} = useContext(AuthContext);
-    
-    return (
-      <nav className={classes.sideNavbar}>
-        <ul>
-          <li>
-            <NavLink to={pathname} activeClassName={classes.active} exact>
-              <IconText icon='fa fa-home' textInfo='Home' />
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to={`${pathname}/profile`}
-              activeClassName={classes.active}
-            >
-              <IconText icon='fa fa-user' textInfo='Profile' />
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to={`${pathname}/reports`}
-              activeClassName={classes.active}
-            >
-              <IconText icon='fa fa-file-text' textInfo='Reports' />
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to={`${pathname}/archives`}
-              activeClassName={classes.active}
-            >
-              <IconText icon='fa fa-archive' textInfo='Archives' />
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
-    );
-}
+const links = {
+  admin: (
+    <>
+      <li>
+        <NavLink to={`${pathname}/reports`} activeClassName={classes.active}>
+          <IconText icon='fa fa-file-text' textInfo='Reports' />
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to={`${pathname}/archives`} activeClassName={classes.active}>
+          <IconText icon='fa fa-archive' textInfo='Archives' />
+        </NavLink>
+      </li>
+    </>
+  ),
+  doctor: (
+    <>
+      <li>
+        <NavLink to={`${pathname}/tasks`} activeClassName={classes.active}>
+          <IconText icon='fa fa-tasks' textInfo='Tasks' />
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to={`${pathname}/questions`} activeClassName={classes.active}>
+          <IconText icon='fa fa-question' textInfo='Questions' />
+        </NavLink>
+      </li>
+    </>
+  ),
+  user: null,
+};
 
-export default SideNavbar
+const SideNavbar = () => {
+  const { userToken } = useContext(AuthContext);
+
+  return (
+    <nav className={classes.sideNavbar}>
+      <ul>
+        <li>
+          <NavLink to={pathname} activeClassName={classes.active} exact>
+            <IconText icon='fa fa-home' textInfo='Home' />
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to={`${pathname}/profile`} activeClassName={classes.active}>
+            <IconText icon='fa fa-user' textInfo='Profile' />
+          </NavLink>
+        </li>
+        {links[userToken.type]}
+      </ul>
+    </nav>
+  );
+};
+
+export default SideNavbar;
--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,11 +1,7 @@
-import DashboardCard from '../../ui/card/dashboard-card/DashboardCard';
 import SideNavbar from '../../components/navbar/side-navbar/SideNavbar';
-import Table from '../../ui/table/Table';
 
 import classes from './Dashboard.module.css';
-import { dummyUserData } from '../../store/dummy-data';
 
-const tableHead = ['User Name', 'User Email','User Type'];
 
 const Dashboard = () => {
     return (
