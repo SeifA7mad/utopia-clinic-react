a37cdaccb52a654bba3500160524626f3637ea45
SeifA7mad <sauofa_ahmad@yahoo.com>
2021-10-05 23:44:56 +0200

    routes for dashboard

---
+++ src/components/dassboard-content/DashboardContent.js
@@ -0,0 +1,34 @@
+import { Switch, Route } from 'react-router-dom';
+import { useContext } from 'react';
+import { AuthContext } from '../../store/auth-context';
+
+const pathname = '/dashboard';
+const routes = {
+  admin: (
+    <>
+      <Route path={`${pathname}/reports`}> </Route>
+      <Route path={`${pathname}/archives`}></Route>
+    </>
+  ),
+  doctor: (
+    <>
+      <Route path={`${pathname}/tasks`}></Route>
+      <Route path={`${pathname}/questions`}></Route>
+    </>
+  ),
+  user: null
+};
+
+
+const DashboardContent = () => {
+  const { userToken } = useContext(AuthContext);
+  return (
+    <Switch>
+      <Route path={`${pathname}`} exact></Route>
+      <Route path={`${pathname}/profile`}></Route>
+      {routes[userToken.type]}
+    </Switch>
+  );
+};
+
+export default DashboardContent;
--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,12 +1,12 @@
 import SideNavbar from '../../components/navbar/side-navbar/SideNavbar';
-
+import DashboardContent from '../../components/dassboard-content/DashboardContent';
 import classes from './Dashboard.module.css';
-
 
 const Dashboard = () => {
     return (
         <div className={classes.dashboard}>
             <SideNavbar />
+            <DashboardContent />
         </div>
     )
 }
